a1329df2a21236304cecb69c7a4a02a652775396
mariogarzac <mariogarzac99@gmail.com>
2023-03-04 16:12:48 -0600

    made improvements

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,10 +5,11 @@ alias gs="git status"
 alias gp="git push"
 
 # projects
-alias py="cd ~/repos/Python/"
-alias dnd="cd ~/Documents/Code/Python/Playground/5eEncounter-cli/"
+alias py="cd ~/repos/Code/Python/"
+alias dnd="cd ~/repos/code/Python/5eEncounter-cli/"
 alias walls="cd ~/repos/Wallpapers/walls"
-alias wallupdate="python3 ~/repos/python/playground/copy.py > ~/repos/Wallpapers/README.md"
+alias wallupdate="python3 ~/repos/code/python/playground/copy.py > ~/repos/Wallpapers/README.md"
+alias compis="cd ~/repos/code/Compiladores"
 
 # utility
 alias hist="history|grep"
@@ -16,6 +17,6 @@ alias cf="cfiles"
 alias la="ls -alF"
 alias vim="nvim"
 alias c="clear"
-alias thm="sudo openvpn ~/Documents/Network/THM/mariomoo.ovpn"
+alias thm="sudo openvpn ~/repos/Network/THM/mariomoo.ovpn"
 
 
--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,5 +1,7 @@
 export EDITOR=nvim
 
+plugins=(git zsh-syntax-highlighting )
+
 export ZSH=$HOME/.oh-my-zsh
 source $ZSH/oh-my-zsh.sh
 
