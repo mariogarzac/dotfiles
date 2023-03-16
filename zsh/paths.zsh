df30a6d1ab1438e15286e2c3492d49bb72bbac14
mariogarzac <mariogarzac99@gmail.com>
2023-03-16 11:23:32 -0600

    added python path

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -6,7 +6,7 @@ export ZSH=$HOME/.oh-my-zsh
 source $ZSH/oh-my-zsh.sh
 
 export PATH="/usr/local/bin:$PATH"
-export PATH="$PATH:/opt/homebrew/opt/node@16/bin"
+export PATH="$PATH:/Users/mariogarza/Library/Python/3.9/bin"
 export PATH="$PATH:/Users/mariogarza/.spicetify"
 
 
