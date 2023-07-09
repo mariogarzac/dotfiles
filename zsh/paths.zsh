e353255bfdeb5349be2d2bafb65a512df707f2b4
mariogarzac <mariogarzac99@gmail.com>
2023-07-09 13:46:43 -0600

    Added python path

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -5,7 +5,6 @@ plugins=(git zsh-syntax-highlighting )
 export ZSH=$HOME/.oh-my-zsh
 source $ZSH/oh-my-zsh.sh
 
-export PATH="/usr/local/bin:$PATH"
-export PATH="$PATH:/Users/mariogarza/Library/Python/3.9/bin"
+export PATH="$PATH:/Users/mariogarza/Library/Python/3.11/bin/"
 
 
