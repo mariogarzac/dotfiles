a71673f72774447619677c18131e92e23a8b305c
mariogarzac <mariogarzac99@gmail.com>
2023-07-09 13:46:31 -0600

    Modified aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,19 +6,14 @@ alias gp="git push"
 
 # projects
 alias repos="cd ~/repos/"
-alias py="cd ~/repos/Code/Python/"
-alias dnd="cd ~/repos/code/Python/5eEncounter-cli/"
 alias walls="cd ~/repos/Wallpapers/walls"
-alias wallupdate="python3 ~/repos/code/python/playground/copy.py > ~/repos/Wallpapers/README.md"
-alias compis="cd ~/repos/code/Compiladores"
 
 # utility
 alias hist="history|grep"
-alias cf="cfiles"
 alias la="ls -alF"
 alias vim="nvim"
 alias c="clear"
-alias thm="sudo openvpn ~/repos/Network/THM/mariomoo.ovpn"
 alias tt="tmux attach-session -t"
+alias tmxc="~/.dotfiles/zsh/tmxc.sh"
 
 
