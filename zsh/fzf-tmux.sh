eb7b8287bb15880b6d9cdf044b0b3e3d8ee04645
mariogarzac <mariogarzac99@gmail.com>
2023-07-18 10:28:35 -0600

    Updated script to open directories and files

--- zsh/fzf-tmux.sh
+++ zsh/fzf-tmux.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
-selected_file=$(find ~/repos ~/Documents ~/Desktop ~/Downloads -type d \( \
+# Select a file but ignore the directories below
+selected_file=$(find ~/repos ~/Documents ~/Desktop ~/Downloads ~/.dotfiles -type d \( \
   -name '.obsidian' \
   -o -name '.git' \
   -o -name 'node_modules' \
@@ -10,20 +11,45 @@ selected_file=$(find ~/repos ~/Documents ~/Desktop ~/Downloads -type d \( \
   -o -name '*Cisco*' \
   \) -prune -o -type f -print | fzf )
 
+
+# Check if input is empty and exit if it is
 if [[ -z "$selected_file" ]]; then
     exit 0
-else
-    file_extension="${selected_file##*.}"
-
-    if [[ $file_extension == "pdf" ]]; then
-        open "$selected_file"
-    else
-        directory=$(dirname "$selected_file")
-        session_name=$(echo "$directory" | awk -F'repos/' '{print $2}' | awk -F'/' '{print $1}')
-
-        cd "$directory"
-        tmux new-session -d -s "$session_name"
-        tmux send-keys -t "$session_name:1" "nvim '$selected_file'" Enter
-        tmux switch-client -t "$session_name"
-    fi
 fi
+
+# Check file extension
+file_extension="${selected_file##*.}"
+
+# Check if selected file is a pdf 
+if [[ $file_extension == "pdf" ]]; then
+    open "$selected_file"
+    exit 0
+fi
+
+# Get directory name and session name
+directory=$(dirname "$selected_file")
+session_name=$(echo "$directory" | awk -F'repos/' '{print $2}' | awk -F'/' '{print $1}')
+
+# Check if session name is empty
+if [[ -z "$session_name" ]]; then
+
+    # If session name is empty then get the directory name after the username
+    session_name=$(echo "$directory" | awk -F $(whoami)'/' '{print $2}' | awk -F '/' '{print $1}')
+
+    # remove the . if there are any
+    session_name=$(echo "$session_name" | sed s/\.//)
+fi
+
+# Change into file's directory
+cd "$directory"
+
+# Create new session
+tmux new-session -d -s "$session_name"
+
+# If . was not passed as an argument, then open the file in vim
+if [[ "$1" != "." ]]; then
+        tmux send-keys -t "$session_name:1" "nvim '$selected_file'" Enter
+fi
+
+# Switch to the new session
+tmux switch-client -t "$session_name"
