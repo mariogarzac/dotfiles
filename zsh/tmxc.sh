90b5bec2a99e5ba4a331972a7180fe3533e82817
mariogarzac <mariogarzac99@gmail.com>
2023-07-20 12:54:58 -0600

    Added option for -n and -s flags

--- zsh/tmxc.sh
+++ zsh/tmxc.sh
@@ -1,30 +1,66 @@
 #!/bin/zsh
 
-if [[ "$1" == "-h" ]]; then
-    echo "usage: tmxc [empty | . | name]"
-    echo "- empty: will set o_o as session name."
-    echo "- .: will set current directory as session name."
-    echo "- name: will set the given name as session name."
-    exit 0
-else
-    # Check if an argument was passed
-    if [[ -z "$1" ]]; then
-        session_name="o_o"
+n_flag=false
+screen_count=1
+session_name="moo"
 
-    elif [[ "$1" = "." ]]; then
-        current_dir=$(pwd)
-
-        # Extract the name of the current folder
-        session_name=$(basename "$current_dir")
-    else
-        session_name="$1"
-    fi
-
+if [[ -z "$1" ]]; then
     tmux new-session -d -s "$session_name"
-
-    # Add a new window to the session
-    tmux new-window -t "$session_name" 
-
-    # Attach to the Tmux session
-    tmux attach-session -t "$session_name" 
+    exit 0
 fi
+
+# Loop through args
+while [[ $# -gt 0 ]]; do
+   case "$1" in 
+        -h)
+          echo "usage: tmxc [empty | -h | -n | -s ]"
+          echo "empty: will set "$session_name" as the session name."
+          echo "-n . will set the session name to the current directory"
+          echo "-n <name> will set the given name as session name."
+          echo "if no name is provided, then it will set the current directory"
+          echo "-s will add windows to the session"
+          exit 0
+          ;;
+
+        -n) 
+          shift
+          if [[ "$1" == "." ]]; then
+            # Extract the name of the current folder
+            session_name=$(basename "$(pwd)")
+          else
+            session_name="$1"
+          fi
+
+          n_flag=true
+          tmux new-session -d -s "$session_name"
+          ;;
+
+        -s) 
+          shift
+          if [[ "$n_flag" = false ]]; then
+            # Extract the name of the current folder
+            session_name=$(basename "$(pwd)")
+            tmux new-session -d -s "$session_name"
+          fi 
+
+          while [[ screen_count -lt "$1" ]]; do
+              tmux new-window -t "$session_name" 
+              (( screen_count++ ))
+          done
+          ;;
+
+        *)
+          echo "Unkown option "$1" "
+          echo "Available options are:"
+          echo "-h for help"
+          echo "-n to set a session name"
+          echo "-s to set amount of windows"
+          exit 0
+          ;;
+   esac
+   shift
+done
+     
+
+# Attach to the Tmux session
+tmux attach-session -t "$session_name" 
