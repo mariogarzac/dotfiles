6b3242141f29c824a1340e9cd3f69d211ecb2b41
mariogarzac <mariogarzac99@gmail.com>
2023-08-06 13:13:59 -0600

    Deleted path

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -5,6 +5,6 @@ plugins=(git zsh-syntax-highlighting )
 export ZSH=$HOME/.oh-my-zsh
 source $ZSH/oh-my-zsh.sh
 
-export PATH="$PATH:/Users/mariogarza/Library/Python/3.11/bin/"
-
+# export PATH="$PATH:/Users/mariogarza/Library/Python/3.11/bin/:/opt/homebrew/bin/go"
+# export PATH="$PATH"
 
