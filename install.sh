8e71b022fa57876978afa903d88a90fd91343cb8
mariogarzac <mariogarzac99@gmail.com>
2023-08-06 13:12:20 -0600

    Update to packages that are installed

--- install.sh
+++ install.sh
@@ -1,13 +1,36 @@
-#!/bin/zsh
+#!/bin/bash
 
 # Install xCode cli tools
 echo "Installing commandline tools..."
 xcode-select --install
 
-# Install Brew
-echo "Installing Brew..."
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew analytics off
+# Install homebrew
+f test ! $(which brew); then
+    echo "Installing homebrew"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+echo "Installing homebrew packages..."
+
+# Install packages
+brew install zsh
+brew install tmux
+brew install neovim
+brew install python3
+brew install zsh-syntax-highlighting
+
+# Install casks
+brew install font-hack-nerd-font
+brew install raycast
+brew install rectangle
+brew install kitty
+brew install docker
+brew install spotify
+
+
+# Clone .dotfiles
+mkdir .dotfiles && cd .dotfiles
+git clone https://github.com/mariogarzac/dotfiles
 
 echo "Changing system settings..."
 defaults write com.apple.dock autohide -bool true
@@ -20,4 +43,5 @@ defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
 defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+
 
