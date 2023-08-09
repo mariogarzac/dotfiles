a1a6e82569dd1a32a5aad9da72edb69b11cccfb9
mariogarzac <mariogarzac99@gmail.com>
2023-08-08 19:07:59 -0600

    fd and ff functions are working. ff still needs work on window naming

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -7,7 +7,55 @@ function del(){
     mv "$@" ~/.Trash
 }
 
+function tm(){
+    session_name="moo"
+    tmux new-session -A -s "$session_name"
+}
+
 function fd(){
+    selected_dir=$(find ~/repos ~/Documents ~/Desktop ~/Downloads ~/.dotfiles -type d \( \
+        -name '.obsidian' \
+        -o -name '.git' \
+        -o -name 'lib' \
+        -o -name 'bin' \
+        -o -name 'node_modules' \
+        -o -name 'Obsidian' \
+        -o -name '_resources' \
+        -o -name 'Labs' \
+        -o -name 'DND' \
+        -o -name 'Network' \
+        -o -name 'Library' \
+        -o -name 'Wallpapers' \
+        -o -name '*Cisco*' \
+        \) -prune -o -type d -print | fzf)
+ 
+    # Check if input is empty and exit if it is
+    if [[ -z "$selected_dir" ]]; then
+        return
+    fi
+
+    session_name=$(basename "$selected_dir")
+    session_name=${session_name//./}
+
+    # Check if the session already exists 
+    if tmux ls | grep -q "$session_name"; then
+        tmux switch -t "$session_name"
+        return
+    fi
+    
+    # Create the session
+    tmux new-session -d -s "$session_name" -c "$selected_dir"
+
+    # Switch or attach to the session
+    if [[ -z "${TMUX}" ]]; then
+        tmux attach -t "$session_name"
+    else
+        tmux switch -t "$session_name"
+    fi
+}
+
+function ff(){
+    session_name="quick"
     selected_file=$(find ~/repos ~/Documents ~/Desktop ~/Downloads ~/.dotfiles -type d \( \
         -name '.obsidian' \
         -o -name '.git' \
@@ -23,105 +71,26 @@ function fd(){
         -o -name 'Wallpapers' \
         -o -name '*Cisco*' \
         \) -prune -o -type f -print | fzf)
-
-    # --preview "echo {} | grep -o '$(read -r input && echo $input)'"
+ 
     # Check if input is empty and exit if it is
     if [[ -z "$selected_file" ]]; then
         return
     fi
 
-    # Check file extension
-    file_extension="${selected_file##*.}"
-
-    # Check if selected file is a pdf 
-    if [[ $file_extension == "pdf" ]]; then
-        open "$selected_file"
-        return
-    fi
-
-    ## Change this to set selected name not directory after code
-
     # Get directory name and session name
-    directory=$(dirname "$selected_file")
-    session_name=$(echo "$directory" | awk -F'Code/' '{print $2}' | awk -F'/' '{print $1}')
-
-    # Check if session name is empty
-    if [[ -z "$session_name" ]]; then
-
-        # If session name is empty then get the directory name after the username
-        session_name=$(echo "$directory" | awk -F $(whoami)'/' '{print $2}' | awk -F '/' '{print $1}')
-
-        # remove the . if there are any
-        session_name=$(echo "$session_name" | sed s/\.//)
-    fi
-
-    # Check if the session already exists
     if tmux ls | grep -q "$session_name"; then
-        tmux switch-client -t "$session_name"
-        return
+        tmux switch -t "$session_name"
+    else
+        tmux new-session -d -s "$session_name" -c "$(dirname $selected_file)"
+        echo "$(basename $selected_file)"
+
+        # Switch or attach to the session
+        if [[ -z "${TMUX}" ]]; then
+            tmux attach -t "$session_name"
+        else
+            tmux switch -t "$session_name"
+        fi
     fi
 
-    # Change into file's directory
-    cd "$directory"
-
-    # Create new session
-    tmux new-session -d -s "$session_name"
-
-    # If . was not passed as an argument, then open the file in vim
-    # if [[ "$1" != "." ]]; then
-    #     tmux send-keys -t "$session_name:1" "nvim '$selected_file'" Enter
-    # fi
-    
-    # Switch to the new session
-    tmux switch-client -t "$session_name"
-        
-}
-
-function tm(){
-    n_flag=false
-    screen_count=1
-    session_name="moo"
-
-    if [[ -z "$1" ]]; then
-        tmux new-session -d -s "$session_name"
-        tmux attach-session -t "$session_name" 
-        return
-    fi
-
-    # Loop through args
-    while [[ $# -gt 0 ]]; do
-        case "$1" in 
-            -n) 
-                shift
-                if [[ "$1" == "." ]]; then
-                    # Extract the name of the current folder
-                    session_name=$(basename "$(pwd)")
-                else
-                    session_name="$1"
-                fi
-
-                n_flag=true
-                tmux new-session -d -s "$session_name"
-                ;;
-
-            -s) 
-                shift
-                if [[ "$n_flag" = false ]]; then
-                    # Extract the name of the current folder
-                    session_name=$(basename "$(pwd)")
-                    tmux new-session -d -s "$session_name"
-                fi 
-
-                while [[ screen_count -lt "$1" ]]; do
-                    tmux new-window -t "$session_name" 
-                    (( screen_count++ ))
-                done
-                ;;
-        esac
-        shift
-    done
-
-    # Attach to the Tmux session
-    tmux attach-session -t "$session_name" 
-
+    tmux send-keys -t "$session_name:1" "nvim '$(basename $selected_file)'" Enter
 }
