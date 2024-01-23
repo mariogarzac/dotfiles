8801015cada37137b09c81a529957fdcf32bdc5b
mariogarzac <mariogarzac99@gmail.com>
2024-01-23 13:02:20 -0600

    Added zshcompdump path

--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -4,6 +4,8 @@ plugins=(git zsh-syntax-highlighting )
 source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 export ZSH=$HOME/.oh-my-zsh
+export ZSH_COMPDUMP=$ZSH/cache/.zcompdump-$HOST
+
 source $ZSH/oh-my-zsh.sh
 
 export PATH="$PATH:/Users/mariogarza/go/bin/"
