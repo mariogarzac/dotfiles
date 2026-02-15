aaf86e7eba08a1dccc7d5c3e36536be13d370140
Mario Garza <mariogarzac99@gmail.com>
2026-02-15 13:21:08 -0600

    Clean up config and modify launch script for polybar in i3 dir

--- config/i3/launch_polybar.sh
+++ config/i3/launch_polybar.sh
@@ -1,9 +1,14 @@
-#!/bin/bash
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar --reload moo &
-  done
-else
-  polybar --reload moo &
-fi
+#!/usr/bin/env bash
 
+# Add this script to your wm startup file.
+
+DIR="$HOME/.config/polybar/"
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch the bar
+polybar -q main -c "$DIR"/config.ini &
